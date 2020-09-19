4bce930aaa4499a0c00e71b5b6aaf11b6c38aba0
Vladimir <torna7@yandex.ru>
2020-09-19 12:32:13 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const inputRub = document.querySelector('#rub'),
+      inputUsd = document.querySelector('#usd');
+
+inputRub.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
+
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+
+    request.addEventListener('load', () => {
+        if (request.status === 200) {
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+        } else {
+            inputUsd.value = "Error";
+        }
+    });
+});
